7a42b5fe78562a408356dc385d7d666d2476b8d3
joshua.riggert <joshua.riggert@cs104u-5a.acad.fullerton.edu>
2020-01-23 15:27:06 -0800

    Made a better hello world.

--- part-2/hello_world.cpp
+++ part-2/hello_world.cpp
@@ -1,11 +1,19 @@
-// Oscar Mayer
-// CPSC 121-99
-// oscar.mayer@csu.fullerton.edu
+// Joshua Riggert
+// CPSC 121-03
+// joshua.riggert@csu.fullerton.edu
 
 #include <iostream>
 using namespace std;
 
 int main(int argc, char *argv[]){
-  cout << "Hello world!" << endl;
+  if (argc != 3) {
+    cout << "Too few arguments.\n";
+    return 1;
+  }
+  string name = argv[1];
+  int repeat = stoi(argv[2]);
+  for(int i = 0; i < repeat; i++){
+    cout << "Hello " << name << "!" << endl;
+  }
   return 0;
 }
